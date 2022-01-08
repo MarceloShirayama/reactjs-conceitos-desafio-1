8361ebb401233b86a2ba355a76411b80b874b4ed
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-01-08 12:10:36 -0300

    initial template

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+const isDev = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  mode: isDev ? "development" : "production",
+  devtool: isDev ? "eval-source-map" : "source-map",
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  devServer: {
+    static: path.resolve(__dirname, "public"),
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?(j|t)sx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [isDev && require.resolve("react-refresh/babel")].filter(
+              Boolean
+            ),
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    isDev && new ReactRefreshWebpackPlugin(),
+  ].filter(Boolean),
+};
